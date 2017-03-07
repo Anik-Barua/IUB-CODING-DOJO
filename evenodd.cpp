bc4eed738158d4569c3149331e577a80af743921
Anik-Barua <anikbaruanirob1212@gmail.com>
2017-03-07 12:26:58 +0600

    1st file upped
    testing

---
+++ evenodd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cout<<"Enter number : "<<endl;
+  cin>>num;
+  if(num % 2 == 0){
+    cout<<"your number is : "<<num<<"  is even  "<<endl;
+  }
+  else{
+    cout<<"your number is : "<<num<<" is odd  "<<endl;
+  }
+
+
+
+
+}
